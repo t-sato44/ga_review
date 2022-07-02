40f02821e494509bad01782dc3d234aed26b0a94
motoki kyan <kyan@ryukyuhub.co.jp>
2022-07-02 22:58:23 +0900

    BootstrapのSASS環境構築完了、テストファイルあり

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/test', function () {
+    return view('test');
+});
+
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
