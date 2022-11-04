9888a63cae522c62f4de8d7c68c8831d17fdf161
motoki kyan <kyan@ryukyuhub.co.jp>
2022-11-05 01:21:55 +0900

    ggg

--- resources/views/components/nav/head.blade.php
+++ resources/views/components/nav/head.blade.php
@@ -9,7 +9,7 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link" href="{{ route('home') }}">TOP</a>
+          <a class="nav-link" href="{{ route('home') }}">TOP2</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="dropdownReview" role="button" data-bs-toggle="dropdown" aria-expanded="false">
